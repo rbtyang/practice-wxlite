28b93cfeb1b9d022231558bc66803dcb7ecedd75
fumingyang <robotyang123@163.com>
2018-11-23 15:27:22 +0800

    feat(npm): 加入有赞小程序UI库 Vant-Weapp 支持

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # practice-wechat-lite
-Code to learn and test Wechat lite features.
+练习和测试微信小程序功能的项目
+
+# install
+- 在项目根目录 执行 `./install.sh`
+- 在项目根目录 执行 `npm install`
+- 在微信开发者工具 选择 “工具 - 构建 npm”，并 ==重新打开== 微信开发者工具
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#花果鲜福利社初始化环境文件
+
+echo "install start ..."
+
+if [ ! -f "project.config.json" ]; then
+    cp project.config.example.json project.config.json
+    echo ">>> project.config.json copy success"
+else
+    echo "project.config.json has exist"
+fi
+
+echo "install successfully !"
